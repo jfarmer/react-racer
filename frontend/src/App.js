a31eac0cbbc9f3cb88e3c74acdf57e1b7e0af2b4
Mac Cowell <macowell@gmail.com>
2019-05-08 22:49:15 -0700

    window.location to get codesandbox url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,8 @@ import './App.css';
 
 // const socket = io('http://localhost:4001');
 // host = 'https://2v0n4v15z0.sse.codesandbox.io'
-const server = process.env.SERVER || 'localhost'
+// const server = process.env.SERVER || 'localhost'
+const server = window.location.href || 'localhost'
 const port = process.env.PORT || '4001'
 const socket = io(`${server}:${port}`);
 
