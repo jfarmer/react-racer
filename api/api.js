58675fa9011c83d8099f87e950f0faacab4ef318
Jesse Farmer <jesse@20bits.com>
2022-10-28 11:24:22 -0700

    Update Socket.IO require statement

--- api/api.js
+++ api/api.js
@@ -1,14 +1,14 @@
 const { v4: uuid } = require('uuid');
 const express = require('express');
 const http = require("http");
-const socketIo = require('socket.io');
+const { Server: SocketIO } = require('socket.io');
 
 const port = process.env.PORT || 4001;
 
 const app = express();
 
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new SocketIO(server);
 
 const RacerStates = Object.freeze({
   WAITING: Symbol('RACER_STATE_WAITING'),
